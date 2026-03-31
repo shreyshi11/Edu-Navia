bbb61970b5817bf46f4044291f04996b1e1073f0
Esha jha <jha.esha2023@gmail.com>
2026-03-31 09:21:39 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Vite looks for .env files automatically
-  // For basic static sites, no extra config is needed
   server: {
     port: 3000,
+    proxy: {
+      '/recommendations': 'http://127.0.0.1:8000',
+      '/predict': 'http://127.0.0.1:8000'
+    }
   },
   build: {
     outDir: 'dist',
