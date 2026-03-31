87785241ca817eb6a7457062bbdc47630ba0154d
Esha Jha <149310101+Eshajha19@users.noreply.github.com>
2026-03-31 13:00:21 +0530

    Remove Firebase API keys from vite.config.js
    Removed sensitive Firebase configuration from the Vite config.

--- vite.config.js
+++ vite.config.js
@@ -12,15 +12,7 @@ htmlFiles.forEach(file => {
 });
 
 export default defineConfig({
-  define: {
-    'import.meta.env.VITE_FIREBASE_API_KEY': JSON.stringify("AIzaSyAPXYOQk3LPxayuymhac79P4QsX13xvMZA"),
-    'import.meta.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify("edu-navigator-cc4a0.firebaseapp.com"),
-    'import.meta.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify("edu-navigator-cc4a0"),
-    'import.meta.env.VITE_FIREBASE_STORAGE_BUCKET': JSON.stringify("edu-navigator-cc4a0.appspot.com"),
-    'import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify("33607968332"),
-    'import.meta.env.VITE_FIREBASE_APP_ID': JSON.stringify("1:33607968332:web:59cbf34a0aae68375736e9"),
-    'import.meta.env.VITE_API_URL': JSON.stringify("https://edunavia.onrender.com")
-  },
+  
   server: {
     port: 3000,
     proxy: {
